bcde153eb93fccb7df494d76d092329f58d860e6
Zuckuu <entbit12@gmail.com>
2022-08-04 13:49:18 -0500

    top portion done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import './App.css';
-import Search from './components/search/search';
+import "./App.css";
+import Search from "./components/search/search";
 
 function App() {
-
-const handleOnSearchChange= (searchData) => {
-  console.log(searchData)
-} 
-
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
+      <div className="greeting_nav">
+        <h1>Hello, How's the weather in...</h1>
+        <a
+          href="https://github.com/Zuckuu/weather-site"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+        <a
+          href="https://zacks-website.vercel.app/"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          For More
+        </a>
+      </div>
+      <Search onSearchChange={handleOnSearchChange} className="search_bar" />
     </div>
   );
 }
