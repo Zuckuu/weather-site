07cfaf4861151b5636170e50971e25e1b12332d1
Zuckuu <entbit12@gmail.com>
2022-08-04 14:34:22 -0500

    adding the current weather

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
@@ -26,6 +27,7 @@ function App() {
         </a>
       </div>
       <Search onSearchChange={handleOnSearchChange} className="search_bar" />
+      <CurrentWeather/>
     </div>
   );
 }
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,13 @@
+import "./current-weather.css"
+
+import React from 'react';
+
+const CurrentWeather = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default CurrentWeather;
