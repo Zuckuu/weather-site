502eeb8ed0b5211a3ac47b4b15e3866ef776c798
Zuckuu <entbit12@gmail.com>
2022-08-05 11:05:30 -0500

    styling the weather chart

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,17 +1,43 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
-import React from 'react';
+import React from "react";
 
 const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">nameofcity</p>
-        <p className="weather-description">desofcity</p>
+        <div>
+          <p className="city">nameofcity</p>
+          <p className="weather-description">desofcity</p>
+        </div>
+        <img className="weather-icon" src="/icons/01d.png" alt="weather" />
       </div>
-      <img className="weather-icon" src="" alt="weather" />
+      <div className="bottom">
+        <p className="temperature">18°C </p>
+        <div className="details">
+          <div className="parameter-row">
+              <span className="parameter-label bold">Details</span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">Feel like</span>
+              <span className="parameter-value">°C</span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">Wind</span>
+              <span className="parameter-value">°C</span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">Humidity</span>
+              <span className="parameter-value">°C</span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">Pressure</span>
+              <span className="parameter-value">°C</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default CurrentWeather;
